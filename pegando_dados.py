e37e62aec9087e063cbda124a3a2481861710ae4
Marcus Vinicius <53979368+marvin-2102@users.noreply.github.com>
2022-06-17 19:08:30 -0300

    Delete pegando_dados.py

--- pegando_dados.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from shutil import move
-
-#Agora vamos pegar esses dados no campo de downloads
-
-caminho = Path('C:/Users/marki/Downloads/USD_BRL Dados Históricos.csv')
-novo_caminho = Path('C:/Users/marki/Documents/Selenium_Modulo/USD_BRL Dados Históricos.csv')
-
-move(caminho, novo_caminho)
