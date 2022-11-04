982599c5ea2ffaf9e99422a6bbe14240f01eafcc
Marcus Vinicius <53979368+marvin-2102@users.noreply.github.com>
2022-11-04 07:50:26 -0300

    Update transPositiva.py

--- OpenCV/MascaraCascade/transPositiva.py
+++ OpenCV/MascaraCascade/transPositiva.py
@@ -4,20 +4,27 @@ from imutils import paths
 import shutil
 
 def listPosImagem():
+
+    # Entra na pasta contendo as imagens positivas
     imagemPath = list(paths.list_images('Imagens/comMascara'))
     numero = 1
+
+    # Cria uma pasta chamada 'pos'
     if not os.path.exists('pos'):
         os.makedirs('pos')
 
+    # Percorre as imagens da pasta e cria uma imagem formatada em escala cinza
     for foto in imagemPath:
-        foto.replace(foto, f"pos/{str(numero)}.png")
-        shutil.copy(foto, foto.replace(foto, f"pos/{str(numero)}.png"))
-        img = cv.imread(f"pos/{str(numero)}.png", cv.IMREAD_GRAYSCALE)
+
+        foto.replace(foto, f"pos/{numero}.png")
+        shutil.copy(foto, foto.replace(foto, f"pos/{numero}.png"))
+        img = cv.imread(f"pos/{numero}.png", cv.IMREAD_GRAYSCALE)
         resized_image = cv.resize(img, (100, 100))
-        cv.imwrite(f"pos/{str(numero)}.png", resized_image)
+        cv.imwrite(f"pos/{numero}.png", resized_image)
 
         print(foto.replace(foto, f"pos/{str(numero)}.png"))
 
         numero += 1
 
+# Chama a função
 listPosImagem()
