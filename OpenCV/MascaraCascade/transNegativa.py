e1ec5e9502d9338682fbfdf9b4281cd380f31178
Marcus Vinicius <53979368+marvin-2102@users.noreply.github.com>
2022-11-04 07:40:15 -0300

    Update transNegativa.py

--- OpenCV/MascaraCascade/transNegativa.py
+++ OpenCV/MascaraCascade/transNegativa.py
@@ -23,24 +23,3 @@ def listNegImagem():
 listNegImagem()
 
 
-
-# def store_raw_images():
-#     neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
-#     neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
-#     pic_num = 1
-#
-#     if not os.path.exists('neg'):
-#         os.makedirs('neg')
-#
-#     for i in neg_image_urls.split('\n'):
-#         try:
-#             print(i)
-#             urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
-#             img = cv.imread("neg/"+str(pic_num)+".jpg",cv.IMREAD_GRAYSCALE)
-#             # should be larger than samples / pos pic (so we can place our image on it)
-#             resized_image = cv.resize(img, (100, 100))
-#             cv.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
-#             pic_num += 1
-#
-#         except Exception as e:
-#             print(str(e))
